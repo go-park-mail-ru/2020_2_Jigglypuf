2fd3436ad0d29988150e9550b4f58b66345d86af
Felix1Green <vvnncc@mail.ru>
2020-10-08 16:49:57 +0300

    trash removed

--- movieService/tests/movieService_test.go
+++ movieService/tests/movieService_test.go
@@ -1,15 +1,3 @@
 package tests
 
 
-cinemaList := "[{\"Id\":1,\"Name\":\"Гренландия\",\"Description\":\"Greenland description\",\"Rating\":0,\"PathToAvatar\"" +
-":\"/media/greenland.jpg\"},{\"Id\":2,\"Name\":\"Антибеллум\",\"Description\":\"Антибеллум description\",\"Rating\"" +
-":0,\"PathToAvatar\":\"/media/antibellum.jpg\"},{\"Id\":3,\"Name\":\"Довод\",\"Description\"" +
-":\"Довод description\",\"Rating\":0,\"PathToAvatar\":\"/media/dovod.jpg\"},{\"Id\":4,\"Name\"" +
-":\"Гнездо\",\"Description\":\"Гнездо description\",\"Rating\":0,\"PathToAvatar\"" +
-":\"/media/gnezdo.jpg\"},{\"Id\":5,\"Name\":\"Сделано в Италии\",\"Description\":\"Италиан description\",\"Rating\"" +
-":0,\"PathToAvatar\":\"/media/italian.jpg\"},{\"Id\":6,\"Name\":\"Мулан\",\"Description\":\"Мулан description\",\"Rating\"" +
-":0,\"PathToAvatar\":\"/media/mulan.jpg\"},{\"Id\":7,\"Name\":\"Никогда всегда всегда никогда\",\"Description\"" +
-":\"Никогда description\",\"Rating\":0,\"PathToAvatar\":\"/media/nikogda.jpg\"},{\"Id\":8,\"Name\":\"После\",\"Description\"" +
-":\"После description\",\"Rating\":0,\"PathToAvatar\":\"/media/posle.jpg\"},{\"Id\":9,\"Name\":\"Стрельцов\",\"Description\"" +
-":\"Стрельцов description\",\"Rating\":0,\"PathToAvatar\":\"/media/strelcov.jpg\"}]"
-
