90a36db68059460b110ad3fdb621020b9937c7a2
Oladiy <bd3396121@gmail.com>
2020-10-12 23:25:43 +0300

    fix bracket

--- internal/pkg/profile/config.go
+++ internal/pkg/profile/config.go
@@ -4,6 +4,6 @@ var (
 	MediaPath       = "/media/"
 	SavingPath      = "../../media"
 	AvatarFormName  = "avatar"
-	NameFormName    = "name"
 	SurnameFormName = "surname"
+	NameFormName    = "name"
 )
