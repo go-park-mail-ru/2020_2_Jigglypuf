98944dcb353221e234a3de17f945036318f26a25
Felix1Green <vvnncc@mail.ru>
2020-10-27 17:17:04 +0300

    [FIX]: docker-compose fixed

--- internal/pkg/authentication/PostgresConfig.go
+++ internal/pkg/authentication/PostgresConfig.go
@@ -3,7 +3,7 @@ package authentication
 const (
 	Host     = "postgresql"
 	Port     = 5432
-	User     = "Backend_cinema_interface"
+	User     = "cinema_backend_interface"
 	Password = "some_password"
 	DBName   = "BackendCinemaInterface"
 )
