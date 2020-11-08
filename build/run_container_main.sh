1da576bbdc91357e272961989013c7186db65eec
Felix1Green <vvnncc@mail.ru>
2020-11-08 15:38:42 +0300

    [FIX]: some internal fixes

--- build/run_container_main.sh
+++ build/run_container_main.sh
@@ -1,2 +1,3 @@
-sudo docker build -f dockerfiles/main.Dockerfile ..
+sudo docker build -t main_image_docker_service -f dockerfiles/main.Dockerfile ..
+sudo docker tag main_image_docker_service felixgreen/cinema_interfaceservice_image
 sudo docker push felixgreen/cinema_interfaceservice_image
