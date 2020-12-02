bab53aba916aad386c65bfb8d6325f02e3edb627
Felix1Green <vvnncc@mail.ru>
2020-12-02 13:04:15 +0300

    [FIX]: errors fixed v3

--- internal/pkg/middleware/monitoring/monitoring_middleware.go
+++ internal/pkg/middleware/monitoring/monitoring_middleware.go
@@ -36,6 +36,7 @@ func AccessLogMiddleware(next http.Handler) http.Handler {
 			handler = handlerName
 			handlerStatus := w.Header().Get(promconfig.StatusNameID)
 			if handlerStatus != "" {
+				log.Println("Status name was found")
 				status = handlerStatus
 				w.Header().Del(promconfig.StatusNameID)
 			}
