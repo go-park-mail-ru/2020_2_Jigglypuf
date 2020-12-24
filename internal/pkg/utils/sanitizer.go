9e011fe9f4dc7acd1926d97a58ad2a360e67795c
Felix1Green <vvnncc@mail.ru>
2020-12-24 13:16:53 +0300

    [FIX]: logs

--- internal/pkg/utils/sanitizer.go
+++ internal/pkg/utils/sanitizer.go
@@ -1,8 +1,12 @@
 package utils
 
-import "github.com/microcosm-cc/bluemonday"
+import (
+	"fmt"
+	"github.com/microcosm-cc/bluemonday"
+)
 
 func SanitizeInput(sanitizer *bluemonday.Policy, arr ...*string) {
+	fmt.Println("Sanitizing input")
 	for index, val := range arr {
 		*arr[index] = sanitizer.Sanitize(*val)
 	}
